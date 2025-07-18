9090c47576e50c315391545fc1c730acf785a39f
siyeoncoding <siso7955@gmail.com>
2025-07-18 13:41:25 +0900

    repository 설정

--- src/main/java/com/studyon/repository/UserRepository.java
+++ src/main/java/com/studyon/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.studyon.repository;
 
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
-    public String save(){
-        return "UserRepository 회원 저장";
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
