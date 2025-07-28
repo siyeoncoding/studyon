e8ffd56412b05c715aea71c08baef5a768063899
siyeoncoding <siso7955@gmail.com>
2025-07-28 21:19:16 +0900

    test

--- src/main/java/com/studyon/controller/UserController.java
+++ src/main/java/com/studyon/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     }
 
 
-    // POST /users/register 설계
+    // POST /users/register
     @PostMapping("/register")
     public User registerUser(@RequestParam String name, @RequestParam String email) {
         return userService.registerUser(name, email);
