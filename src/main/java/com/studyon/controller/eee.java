bdf3859ed4477b60349cc94fbe3097f6fd2d431b
siyeoncoding <siso7955@gmail.com>
2025-08-10 21:41:14 +0900

    test

---
+++ src/main/java/com/studyon/controller/eee.java
@@ -0,0 +1,29 @@
+package com.studyon.controller;
+
+import com.studyon.entitiy.User;
+import com.studyon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class eee {
+
+    private final UserService userService;
+
+    //자동
+    @Autowired
+    public eee(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    // POST /users/registe
+    @PostMapping("/register")
+    public User registerUser(@RequestParam String name, @RequestParam String email) {
+        return userService.registerUser(name, email);
+    }
+}
